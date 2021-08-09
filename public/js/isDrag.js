8a68db1994e235932d66a9ff610cf99e718b5780
Lockmer <rickylockmer@gmail.com>
2021-08-09 16:35:45 -0600

    first commit

---
+++ public/js/isDrag.js
@@ -0,0 +1,35 @@
+let isDown = false;
+let startX;
+let scrollLeft;
+
+$('.scroll').on('mouseenter', function () {
+
+    this.addEventListener("mousedown", e => {
+        isDown = true;
+        this.classList.add("active");
+        startX = e.pageX - this.offsetLeft;
+        scrollLeft = this.scrollLeft;
+    });
+    this.addEventListener("mouseleave", () => {
+        isDown = false;
+        this.classList.remove("active");
+    });
+    this.addEventListener("mouseup", () => {
+        isDown = false;
+        this.classList.remove("active");
+    });
+    this.addEventListener("mousemove", e => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - this.offsetLeft;
+        const walk = x - startX;
+        this.scrollLeft = scrollLeft - walk;
+    });
+
+}
+);
+
+
+
+
+
